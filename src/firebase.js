bb237fa4886db47b24169a111b84e683ad6f9328
Sreenath <sreenathkumar66@gmail.com>
2023-08-05 22:53:33 +0600

    fix: database connections

--- src/firebase.js
+++ src/firebase.js
@@ -4,16 +4,15 @@ import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { useState, useEffect } from "react";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA4rbdtiCPN2TODMauKYhlVpMvB6uTjMv8",
-  authDomain: "udemy-clone-9dfa6.firebaseapp.com",
-  databaseURL:
-    "https://udemy-clone-9dfa6-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "udemy-clone-9dfa6",
-  storageBucket: "udemy-clone-9dfa6.appspot.com",
-  messagingSenderId: "728654922704",
-  appId: "1:728654922704:web:9f16f203b6ca80d701afd5",
-  measurementId: "G-GLT6MJ4WQD",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
